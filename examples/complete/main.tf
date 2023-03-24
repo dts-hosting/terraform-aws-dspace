31c8059964a9542191bd04f053e953710232086f
Mark Cooper <mark.c.cooper@outlook.com>
2023-03-24 12:25:08 -0700

    Add cluster to complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -254,3 +254,42 @@ module "efs" {
 
   tags = local.tags
 }
+
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 4.0"
+
+  cluster_name = local.name
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.this.name
+      }
+    }
+  }
+
+  # Capacity provider
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/ecs/${local.name}"
+  retention_in_days = 7
+
+  tags = local.tags
+}
