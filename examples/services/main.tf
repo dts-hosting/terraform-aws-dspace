ea2948ba3cce45a077123426c25ae877fb925beb
Mark Cooper <mark.c.cooper@outlook.com>
2023-03-29 23:36:56 -0700

    Start adding primary data sources

--- examples/services/main.tf
+++ examples/services/main.tf
@@ -9,13 +9,6 @@ provider "aws" {
   alias   = "dns"
 }
 
-data "aws_availability_zones" "available" {}
-data "aws_caller_identity" "current" {}
-data "aws_route53_zone" "selected" {
-  provider = aws.dns
-  name     = "${var.domain}."
-}
-
 locals {
   name   = "dspace-ex-${basename(path.cwd)}"
   region = "us-west-2"
--- examples/services/variables.tf
+++ examples/services/variables.tf
@@ -21,3 +21,66 @@ variable "profile_for_dns" {
 variable "solr_img" {
   default = "dspace/dspace-solr:dspace-7_x"
 }
+
+data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
+data "aws_route53_zone" "selected" {
+  provider = aws.dns
+  name     = "${var.domain}."
+}
+
+################################################################################
+# External resources
+################################################################################
+variable "cluster_name" {}
+variable "efs_name" {}
+variable "lb_name" {}
+variable "security_group_name" {}
+variable "service_discovery_namespace" {}
+variable "subnet_type" {}
+variable "vpc_name" {}
+
+data "aws_ecs_cluster" "selected" {
+  cluster_name = var.cluster_name
+}
+
+data "aws_efs_file_system" "selected" {
+  tags = {
+    Name = var.efs_name
+  }
+}
+
+data "aws_lb" "selected" {
+  name = var.lb_name
+}
+
+data "aws_lb_listener" "selected" {
+  load_balancer_arn = data.aws_lb.selected.arn
+  port              = 443
+}
+
+data "aws_security_group" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.security_group_name]
+  }
+}
+
+data "aws_service_discovery_dns_namespace" "selected" {
+  name = var.service_discovery_namespace
+  type = "DNS_PRIVATE"
+}
+
+data "aws_subnets" "selected" {
+  filter {
+    name   = "tag:Type"
+    values = [var.subnet_type]
+  }
+}
+
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
