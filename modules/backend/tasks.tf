d8716a2fe3ce9ceccd4905ab30768e29d1d031fc
Mark Cooper <mark.c.cooper@outlook.com>
2023-04-02 13:31:20 -0700

    Add tasks var & resources, cleanup docs

---
+++ modules/backend/tasks.tf
@@ -0,0 +1,39 @@
+resource "aws_cloudwatch_event_rule" "this" {
+  for_each = var.tasks
+
+  name                = "${var.name}-${each.key}"
+  schedule_expression = each.value.schedule
+}
+
+resource "aws_cloudwatch_event_target" "this" {
+  for_each = var.tasks
+
+  target_id = "${var.name}-${each.key}"
+  arn       = var.cluster_id
+  rule      = aws_cloudwatch_event_rule.this[each.key].name
+  role_arn  = aws_iam_role.this.arn
+
+  ecs_target {
+    launch_type         = "FARGATE"
+    platform_version    = "LATEST"
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.this["cli"].arn
+
+    network_configuration {
+      assign_public_ip = var.assign_public_ip
+      security_groups  = [var.security_group_id]
+      subnets          = var.subnets
+    }
+  }
+
+  input = <<DOC
+{
+  "containerOverrides": [
+    {
+      "name": "cli",
+      "command": ${jsonencode(each.value.args)}
+    }
+  ]
+}
+DOC
+}
